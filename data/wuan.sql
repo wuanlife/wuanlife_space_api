8350c8c8511210fdcb19b3096ab0fcd34f4b2d11
aunhappy <1195417752@qq.com>
2016-05-06 19:25:16 +0800

    更换分支

--- data/wuan.sql
+++ data/wuan.sql
@@ -14,10 +14,10 @@
 --
 
 CREATE TABLE IF NOT EXISTS `group_base` (
-  `ID` int(4) unsigned NOT NULL AUTO_INCREMENT COMMENT '组ID',
+  `id` int(4) unsigned NOT NULL AUTO_INCREMENT COMMENT '组id',
   `name` varchar(11) CHARACTER SET gbk NOT NULL UNIQUE COMMENT '组名',
   `delete` int(1) NOT NULL DEFAULT '0' COMMENT '删除',
-  PRIMARY KEY (`ID`)
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='组表' AUTO_INCREMENT=4 ;
 
 
@@ -29,8 +29,8 @@ CREATE TABLE IF NOT EXISTS `group_base` (
 --
 
 CREATE TABLE IF NOT EXISTS `group_detail` (
-  `ID` int(4) unsigned NOT NULL COMMENT '组ID',
-  `userID` int(5) unsigned NOT NULL COMMENT '成员ID',
+  `group_base_id` int(4) unsigned NOT NULL COMMENT '组id',
+  `user_base_id` int(5) unsigned NOT NULL COMMENT '成员id',
   `authorization` varchar(9) CHARACTER SET utf8 NOT NULL COMMENT '身份'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='组成员表';
 
@@ -42,15 +42,15 @@ CREATE TABLE IF NOT EXISTS `group_detail` (
 --
 
 CREATE TABLE IF NOT EXISTS `post_base` (
-  `ID` int(9) unsigned NOT NULL AUTO_INCREMENT COMMENT '帖子ID',
-  `userID` int(5) unsigned NOT NULL COMMENT '发帖人ID',
-  `groupID` int(4) unsigned NOT NULL COMMENT '组ID',
+  `id` int(9) unsigned NOT NULL AUTO_INCREMENT COMMENT '帖子id',
+  `user_base_id` int(5) unsigned NOT NULL COMMENT '发帖人id',
+  `group_base_id` int(4) unsigned NOT NULL COMMENT '组id',
   `title` varchar(30) CHARACTER SET gbk NOT NULL COMMENT '标题',
   `digest` int(1) NOT NULL DEFAULT '0' COMMENT '精华',
   `sticky` int(1) NOT NULL DEFAULT '0' COMMENT '置顶',
   `delete` int(1) NOT NULL DEFAULT '0' COMMENT '删除',
-  PRIMARY KEY (`ID`),
-  KEY `userID` (`userID`,`groupID`)
+  PRIMARY KEY (`id`),
+  KEY `user_base_id` (`user_base_id`,`group_base_id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='主帖' AUTO_INCREMENT=13 ;
 
 
@@ -62,15 +62,15 @@ CREATE TABLE IF NOT EXISTS `post_base` (
 --
 
 CREATE TABLE IF NOT EXISTS `post_detail` (
-  `ID` int(5) unsigned NOT NULL COMMENT '帖子ID',
-  `postID` int(5) unsigned NOT NULL COMMENT '回帖人ID',
-  `replyID` int(5) unsigned DEFAULT NULL COMMENT '回复的ID',
+  `post_base_id` int(5) unsigned NOT NULL COMMENT '帖子id',
+  `user_base_id` int(5) unsigned NOT NULL COMMENT '回帖人id',
+  `replyid` int(5) unsigned DEFAULT NULL COMMENT '回复的id',
   `text` varchar(140) COLLATE utf8_bin NOT NULL COMMENT '内容',
   `floor` int(4) NOT NULL COMMENT '楼层',
   `createTime` varchar(20) COLLATE utf8_bin NOT NULL COMMENT '发布时间',
   `delete` int(1) NOT NULL DEFAULT '0' COMMENT '删除',
-  PRIMARY KEY (`ID`,`floor`),
-  KEY `postID` (`postID`,`replyID`)
+  PRIMARY KEY (`post_base_id`,`floor`),
+  KEY `user_base_id` (`user_base_id`,`replyid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='回复帖';
 
 
@@ -82,11 +82,11 @@ CREATE TABLE IF NOT EXISTS `post_detail` (
 --
 
 CREATE TABLE IF NOT EXISTS `user_base` (
-  `ID` int(5) unsigned NOT NULL AUTO_INCREMENT COMMENT '用户ID',
+  `id` int(5) unsigned NOT NULL AUTO_INCREMENT COMMENT '用户id',
   `password` varchar(35) COLLATE utf8_bin NOT NULL COMMENT '密码',
-  `nickName` varchar(20) COLLATE utf8_bin NOT NULL UNIQUE COMMENT '昵称',
+  `nickname` varchar(20) COLLATE utf8_bin NOT NULL UNIQUE COMMENT '昵称',
   `Email` varchar(30) COLLATE utf8_bin NOT NULL UNIQUE COMMENT '邮箱',
-  PRIMARY KEY (`ID`)
+  PRIMARY KEY (`id`)
 
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='用户表基本' AUTO_INCREMENT=13 ;
 
@@ -99,11 +99,11 @@ CREATE TABLE IF NOT EXISTS `user_base` (
 --
 
 CREATE TABLE IF NOT EXISTS `user_detail` (
-  `ID` int(5) unsigned NOT NULL COMMENT '用户ID',
+  `user_base_id` int(5) unsigned NOT NULL COMMENT '用户id',
   `authorization` varchar(9) CHARACTER SET utf8 NOT NULL COMMENT '身份',
   `status` int(1) NOT NULL COMMENT '状态',
   `lastLogTime` datetime NOT NULL COMMENT '上次登录',
-  PRIMARY KEY (`ID`)
+  PRIMARY KEY (`user_base_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='用户详情';
 
 
