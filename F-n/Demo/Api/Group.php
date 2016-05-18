f1d40599121a25bb38f9c9626c6554d7e285be2f
Finite-Night <190264077@qq.com>
2016-05-18 23:05:12 +0900

    5.18

---
+++ F-n/Demo/Api/Group.php
@@ -0,0 +1,362 @@
+﻿<?php 
+/**
+* 星球接口类
+*/
+class Api_Group extends PhalApi_Api
+{
+    public function getRules(){
+        return array(
+            'create' => array(
+                'user_id'    => array(
+                    'name'    => 'user_id',
+                    'type'    => 'int',
+                    'require' => true,
+                    'desc'    => '用户id',
+                ),
+                'g_name'    => array(
+                    'name'    => 'name',
+                    'type'    => 'string',
+                    'require' => true,
+                    'min'     => '1',
+                    'max'     => '80',
+                    'desc'    => '星球名称',
+                ),
+            ),
+
+            'join' => array(
+                'user_id'    => array(
+                    'name'    => 'user_id',
+                    'type'    => 'int',
+                    'require' => true,
+                    'desc'    => '用户id',
+                ),
+                'g_id' => array(
+                    'name' => 'group_base_id',
+                    'type' => 'int',
+                    'require' => true,
+                    'min' => '1',
+                    'desc' => '星球ID',
+                ),
+            ),
+
+            'gStatus' => array(
+                'user_id'    => array(
+                    'name'    => 'user_id',
+                    'type'    => 'int',
+                    'require' => true,
+                    'desc'    => '用户id',
+                ),
+                'g_id' => array(
+                    'name'    => 'group_base_id',
+                    'type'    => 'int',
+                    'require' => true,
+                    'min'     => '1',
+                    'desc'    => '星球ID',
+                ),
+            ),
+
+            'uStatus' => array(
+                'user_id'    => array(
+                    'name'    => 'user_id',
+                    'type'    => 'int',
+                    'require' => true,
+                    'desc'    => '用户id',
+                ),
+            ),
+
+            'lists' => array(
+                'page' => array(
+                    'name' => 'page',
+                    'type' => 'int',
+                    'require' => false,
+                    'default' => 1,
+                    'desc' => '当前页面',
+                ),
+            ),
+
+            'posts' => array(
+                'user_id'    => array(
+                    'name'    => 'user_id',
+                    'type'    => 'int',
+                    'require' => true,
+                    'desc'    => '用户id',
+                ),
+                'g_id' => array(
+                    'name' => 'group_base_id',
+                    'type' => 'int',
+                    'min' => '1',
+                    'require' => true,
+                    'desc' => '发帖星球',
+                ),
+                'title' => array(
+                    'name' => 'title',
+                    'type' => 'string',
+                    'min' => '1',
+                    'require' => true,
+                    'desc' => '帖子标题',
+                ),
+                'text' => array(
+                    'name' => 'text',
+                    'type' => 'string',
+                    'min' => '1',
+                    'require' => true,
+                    'desc' => '帖子正文',
+                ),
+            ),
+        	
+        	'sPost' => array(
+        			'user_id'    => array(
+        					'name'    => 'user_id',
+        					'type'    => 'int',
+        					'require' => true,
+        					'desc'    => '用户id',
+        			),
+        			'post_id'    => array(
+        					'name'    => 'post_id',
+        					'type'    => 'int',
+        					'require' => true,
+        					'desc'    => '帖子id',
+        			),
+        	),
+        		
+        	'unSPost' => array(
+        			'user_id'    => array(
+        					'name'    => 'user_id',
+        					'type'    => 'int',
+        					'require' => true,
+        					'desc'    => '用户id',
+        			),
+        			'post_id'    => array(
+        					'name'    => 'post_id',
+        					'type'    => 'int',
+        					'require' => true,
+        					'desc'    => '帖子id',
+        			),
+        	),
+        		
+        	'dPost' => array(
+        			'user_id'    => array(
+        					'name'    => 'user_id',
+        					'type'    => 'int',
+        					'require' => true,
+        					'desc'    => '用户id',
+        			),
+        			'post_id'    => array(
+        					'name'    => 'post_id',
+        					'type'    => 'int',
+        					'require' => true,
+        					'desc'    => '帖子id',
+        			),
+        	),
+        		
+        );
+    }
+
+    /**
+     * 星球创建接口
+     * @desc 用于创建星球
+     * @return int code 操作码，1表示创建成功，0表示创建失败
+     * @return object info 星球信息对象
+     * @return int info.group_base_id 星球ID
+     * @return string info.user_base_id 创建者ID
+     * @return string info.name 星球名称
+     * @return string msg 提示信息
+     */
+    public function create(){
+        $rs = array();
+
+        $data = array(
+            'user_id' => $this->user_id,
+            'name'    => $this->g_name,
+        );
+
+        $domain = new Domain_Group();
+        $rs = $domain->create($data);
+
+
+        return $rs;
+    }
+
+    /**
+     * 加入星球接口
+     * @desc 用户加入星球
+     * @return int code 操作码，1表示加入成功，0表示加入失败
+     * @return object info 星球信息对象
+     * @return int info.group_base_id 加入星球ID
+     * @return string info.user_base_id 加入者ID
+     * @return string msg 提示信息
+     */
+    public function join(){
+        $rs = array();
+        $data = array(
+            'user_id' => $this->user_id,
+            'g_id'    => $this->g_id,
+        );
+        $domain = new Domain_Group();
+        $rs = $domain->join($data);
+
+        return $rs;
+    }
+
+    /**
+     * 判断用户登陆状态
+     * @desc 判断是否登录
+     * @return int code 操作码，1表示已登录，0表示未登录
+     * @return object info 状态信息对象
+     * @return int info.id 用户ID
+     * @return string info.nickname 用户昵称
+     * @return string msg 提示信息
+     */
+    public function uStatus(){
+        $rs = array();
+        $data = array(
+            'user_id' => $this->user_id,
+        );
+        $domain = new Domain_Group();
+        $rs = $domain->uStatus($data);
+
+        return $rs;
+    }
+
+    /**
+     * 判断用户是否加入该星球
+     * @desc 判断用户是否加入该星球
+     * @return int code 操作码，1表示已加入，0表示未加入
+     * @return string msg 提示信息
+     */
+    public function gStatus(){
+        $rs = array();
+        $data = array(
+            'user_id' => $this->user_id,
+            'g_id'    => $this->g_id,
+        );
+        $domain = new Domain_Group();
+        $rs = $domain->gStatus($data);
+
+        return $rs;
+    }
+
+    // /**
+    //  * 登出接口
+    //  * @desc 注销
+    //  * @return_ int code 操作码，1表示注销成功，0表示注销失败
+    //  */
+    // public function loginOut(){
+
+    //  $domain = new Domain_Group();
+    //  $domain->out();
+    // }
+
+    /**
+     * 星球列表
+     * @desc 按成员数降序显示星球列表
+     * @return int lists 星球列表对象
+     * @return int lists.name 星球名称
+     * @return int lists.id 星球ID
+     * @return int lists.num 星球成员数
+     * @return int pageCount 总页数
+     * @return int currentPage 当前页
+     */
+    public function lists(){
+        $rs = array(
+            'lists'  => array(),
+            );
+        $pages = 30;                //每页数量
+        $domain = new Domain_Group();
+        $rs['lists'] =  $domain->lists($this->page, $pages);
+        $rs['pageCount'] = $domain->pages['pageCount'];
+        $rs['currentPage'] = $domain->pages['currentPage'];
+        return $rs;
+    }
+
+    /**
+     * 帖子发布
+     * @desc 星球帖子发布
+     * @return int code 操作码，1表示发布成功，0表示发布失败
+     * @return object info 帖子信息对象
+     * @return int info.group_base_id 帖子所属星球ID
+     * @return int info.post_base_id 帖子ID
+     * @return string info.text 帖子正文
+     * @return int info.floor 帖子楼层
+     * @return string info.createTime 帖子发布时间
+     * @return string info.title 帖子标题
+     * @return string msg 提示信息
+     */
+    public function posts(){
+        $rs = array();
+        $data = array(
+                'user_id'       => $this->user_id,
+                'group_base_id' => $this->g_id,
+                'title'         => $this->title,
+                'text'          => $this->text,
+            );
+
+        $domain = new Domain_Group();
+        $rs = $domain->posts($data);
+
+        return $rs;
+    }
+    
+    /**
+     * 置顶帖子
+     * @desc 帖子置顶
+     * @return int code 操作码，1表示操作成功，0表示操作失败
+     * @return string re 提示信息
+     */
+    public function sPost(){
+    	$rs = array();
+    	$data = array(
+    			'user_id'       => $this->user_id,
+    			'post_id'       => $this->post_id,
+    	);
+    
+    	$domain = new Domain_Group();
+    	$rs = $domain->sPost($data);
+    
+    	return $rs;
+    }
+    
+    /**
+     * 取消置顶帖子
+     * @desc 取消帖子置顶
+     * @return int code 操作码，1表示操作成功，0表示操作失败
+     * @return string re 提示信息
+     */
+    public function unSPost(){
+    	$rs = array();
+    	$data = array(
+    			'user_id'       => $this->user_id,
+    			'post_id'       => $this->post_id,
+    	);
+    
+    	$domain = new Domain_Group();
+    	$rs = $domain->unSPost($data);
+    
+    	return $rs;
+    }
+    
+    /**
+     * 删除帖子
+     * @desc 删除帖子
+     * @return int code 操作码，1表示操作成功，0表示操作失败
+     * @return string re 提示信息
+     */
+    public function dPost(){
+    	$rs = array();
+    	$data = array(
+    			'user_id'       => $this->user_id,
+    			'post_id'       => $this->post_id,
+    	);
+    
+    	$domain = new Domain_Group();
+    	$rs = $domain->dPost($data);
+    
+    	return $rs;
+    }
+    
+}
+
+
+
+
+ ?>
---
+++ F-n/Demo/Model/Group.php
@@ -0,0 +1,142 @@
+﻿<?php 
+/**
+* 星球相关DB操作
+*/
+class Model_Group extends PhalApi_Model_NotORM{
+
+	public function checkName($g_name){
+		return $this->getORM()->select('id')->where('name = ?', $g_name)->fetchOne();
+	}
+
+	public function checkGroup($userID, $groupID){
+		return DI()->notorm->group_detail->select('group_base_id')->where('user_base_id = ?', $userID)->where('group_base_id = ?', $groupID)->fetchOne();
+	}
+
+	public function getUser($user_id){
+		return DI()->notorm->user_base->select('nickname')->where('id = ?', $user_id)->fetchOne();
+	}
+
+	public function getAllNum(){
+		return $this->getORM()->count('id');
+	}
+
+	public function lists($limit_st, $page_num){
+		$sql='SELECT gb.name,gb.id,COUNT(gd.user_base_id) AS num FROM group_detail gd, group_base gb '
+			.'where gb.id = gd.group_base_id '
+            .'GROUP BY gd.group_base_id HAVING COUNT(gd.user_base_id)>=1 '
+            .'ORDER BY COUNT(gd.user_base_id) DESC '
+            .'LIMIT :limit_st,:page_num';
+        $params = array(':limit_st' => $limit_st, ':page_num' => $page_num);
+        return $this->getORM()->queryAll($sql, $params);
+	}
+
+	public function add($table,$data){
+		return DI()->notorm->$table->insert($data);
+	}
+
+
+    protected function getTableName($id){
+        return 'group_base';
+    }
+	
+    public function sPost($data){
+    	$rs = array();
+    	
+    	$sqla=DI()->notorm->post_base
+    	->select('group_base_id')
+    	->where('id=?',$data['post_id'])
+    	->fetchone();
+    	
+    	$sqlb=DI()->notorm->group_detail
+    	->select('user_base_id')
+    	->where('group_base_id=?',$sqla['group_base_id'])
+    	->fetchone();
+    	
+    	$s_data = array(
+    			'sticky' => '1',
+    	);
+    	
+    	if($data['user_id']==$sqlb['user_base_id']) {
+    		$s = DI()->notorm->post_base
+    		->where('id =?', $data['post_id'])
+    		->update($s_data);
+    		$rs['code']=1;
+    		$rs['re']="操作成功";
+    	}else{
+            $rs['code']=0;
+            $rs['re']="仅星球创建者能置顶帖子!";
+        }   	
+    	return $rs;
+    }
+    
+    public function unSPost($data){
+    	$rs = array();
+    	 
+    	$sqla=DI()->notorm->post_base
+    	->select('group_base_id')
+    	->where('id=?',$data['post_id'])
+    	->fetchone();
+    	 
+    	$sqlb=DI()->notorm->group_detail
+    	->select('user_base_id')
+    	->where('group_base_id=?',$sqla['group_base_id'])
+    	->fetchone();
+    	 
+    	$s_data = array(
+    			'sticky' => '0',
+    	);
+    	 
+    	if($data['user_id']==$sqlb['user_base_id']) {
+    		$s = DI()->notorm->post_base
+    		->where('id =?', $data['post_id'])
+    		->update($s_data);
+    		$rs['code']=1;
+    		$rs['re']="操作成功";
+    	}else{
+    		$rs['code']=0;
+    		$rs['re']="仅星球创建者能取消置顶帖子!";
+    	}
+    	return $rs;
+    }
+    
+    public function dPost($data){
+    	$rs = array();
+    
+    	$sqla=DI()->notorm->post_base
+    	->select('group_base_id')
+    	->where('id=?',$data['post_id'])
+    	->fetchone();
+    
+    	$sqlb=DI()->notorm->group_detail
+    	->select('user_base_id')
+    	->where('group_base_id=?',$sqla['group_base_id'])
+    	->fetchone();
+    
+    	$d_data = array(
+    			'`delete`' => '1',
+    	);
+    
+    	if($data['user_id']==$sqlb['user_base_id']) {
+    		$sa = DI()->notorm->post_base
+    		->where('id =?', $data['post_id'])
+    		->update($d_data);
+    		$sb = DI()->notorm->post_detail
+    		->where('post_base_id=?', $data['post_id'])
+    		->update($d_data);
+    		$rs['code']=1;
+    		$rs['re']="操作成功";
+    	}else{
+    		$rs['code']=0;
+    		$rs['re']="仅星球创建者能删除帖子!";
+    	}
+    	return $rs;
+    }
+    
+}
+
+
+
+
+
+
+ ?>
