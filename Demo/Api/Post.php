eecca17f50575bc78c3b0cf512a43a864e07c2bf
xiaoxinTOM <xiaoxin1995ok@gmail.com>
2016-05-22 23:07:56 +0800

    接口列表返回参数添加digest（加精）

--- Demo/Api/Post.php
+++ Demo/Api/Post.php
@@ -1,4 +1,4 @@
-<?php
+﻿<?php
 /**
  * 数据库接口服务类
  */
@@ -34,7 +34,51 @@ class Api_Post extends PhalApi_Api{
                 'post_base_id' => array('name' => 'post_id', 'type' => 'int', 'require' => true, 'desc' => '帖子ID'),
                 'title' => array('name' => 'title', 'type' => 'string', 'min' => '1','require' => true, 'desc' => '帖子标题'),
                 'text' => array('name' => 'text', 'type' => 'string', 'min' => '1','require' => true, 'desc' => '帖子内容'),
-            )
+            ),
+	    	'stickyPost' => array(
+        			'user_id'    => array(
+        					'name'    => 'user_id',
+        					'type'    => 'int',
+        					'require' => true,
+        					'desc'    => '用户id',
+        			),
+        			'post_id'    => array(
+        					'name'    => 'post_id',
+        					'type'    => 'int',
+        					'require' => true,
+        					'desc'    => '帖子id',
+        			),
+        	),
+        		
+        	'unStickyPost' => array(
+        			'user_id'    => array(
+        					'name'    => 'user_id',
+        					'type'    => 'int',
+        					'require' => true,
+        					'desc'    => '用户id',
+        			),
+        			'post_id'    => array(
+        					'name'    => 'post_id',
+        					'type'    => 'int',
+        					'require' => true,
+        					'desc'    => '帖子id',
+        			),
+        	),
+        		
+        	'deletePost' => array(
+        			'user_id'    => array(
+        					'name'    => 'user_id',
+        					'type'    => 'int',
+        					'require' => true,
+        					'desc'    => '用户id',
+        			),
+        			'post_id'    => array(
+        					'name'    => 'post_id',
+        					'type'    => 'int',
+        					'require' => true,
+        					'desc'    => '帖子id',
+        			),
+        	),
         );
     }
 
@@ -72,6 +116,7 @@ class Api_Post extends PhalApi_Api{
      * @return string posts.groupName 星球名称
      * @return int pageCount 总页数
      * @return int currentPage 当前页
+     * @return int digest 加精
      */
     public function getGroupPost(){
         $data   = array();
@@ -200,4 +245,61 @@ class Api_Post extends PhalApi_Api{
         $rs = $domain->editPost($data);
         return $rs;
     }
-}
+    /**
+     * 置顶帖子
+     * @desc 帖子置顶
+     * @return int code 操作码，1表示操作成功，0表示操作失败
+     * @return string re 提示信息
+     */
+    public function stickyPost(){
+    	$rs = array();
+    	$data = array(
+    			'user_id'       => $this->user_id,
+    			'post_id'       => $this->post_id,
+    	);
+    
+    	$domain = new Domain_Post();
+    	$rs = $domain->stickyPost($data);
+    
+    	return $rs;
+    }
+    
+    /**
+     * 取消置顶帖子
+     * @desc 取消帖子置顶
+     * @return int code 操作码，1表示操作成功，0表示操作失败
+     * @return string re 提示信息
+     */
+    public function unStickyPost(){
+    	$rs = array();
+    	$data = array(
+    			'user_id'       => $this->user_id,
+    			'post_id'       => $this->post_id,
+    	);
+    
+    	$domain = new Domain_Post();
+    	$rs = $domain->unStickyPost($data);
+    
+    	return $rs;
+    }
+    
+    /**
+     * 删除帖子
+     * @desc 删除帖子
+     * @return int code 操作码，1表示操作成功，0表示操作失败
+     * @return string re 提示信息
+     */
+    public function deletePost(){
+    	$rs = array();
+    	$data = array(
+    			'user_id'       => $this->user_id,
+    			'post_id'       => $this->post_id,
+    	);
+    
+    	$domain = new Domain_Post();
+    	$rs = $domain->deletePost($data);
+    
+    	return $rs;
+    }
+}  
+    
