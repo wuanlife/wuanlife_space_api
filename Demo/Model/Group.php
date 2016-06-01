a987ed8c20c0823b3b05da0481219a835edf6b88
jinchaojian <1139883346@qq.com>
2016-06-01 18:28:38 +0800

    o

--- Demo/Model/Group.php
+++ Demo/Model/Group.php
@@ -57,8 +57,8 @@ class Model_Group extends PhalApi_Model_NotORM{
 		$params = array(':limit_st' => $limit_st, ':page_num' => $page_num);
 		$re=$this->getORM()->queryAll($sql, $params);
 	}else{
-			$re['name']=NULl;
-			$re['id']=NULl;
+			$re['name']=NULL;
+			$re['id']=NULL;
 			$re['g_image']=NULL;
 			$re['g_introduction']=NULL;
 			$re['num']=0;	
@@ -85,8 +85,8 @@ class Model_Group extends PhalApi_Model_NotORM{
 
 		$re=$this->getORM()->queryAll($sql, $params);
 		}else{
-			$re['name']=NULl;
-			$re['id']=NULl;
+			$re['name']=NULL;
+			$re['id']=NULL;
 			$re['g_image']=NULL;
 			$re['g_introduction']=NULL;
 			$re['num']=0;
