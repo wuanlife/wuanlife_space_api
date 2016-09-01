9445940afd5aa9dd7767b0c660b47ecaf185c42a
aunhappy <1195417752@qq.com>
2016-09-01 10:27:49 +0800

    验证码数据库文件修改

--- data/update_user.sql
+++ data/update_user.sql
@@ -1,6 +1,14 @@
 --2016/08/07
---增加注册时间、验证码有效期、重置密码验证码、验证邮箱验证码
-ALTER TABLE `user_base`  ADD `regtime` INT NOT NULL COMMENT '注册时间',  
-ADD `getpasstime` INT NOT NULL COMMENT '验证码有效期',  
-ADD `p_code` INT(5) NOT NULL DEFAULT '1' COMMENT '重置密码验证码',  
-ADD `e_code` INT(5) NOT NULL DEFAULT '1' COMMENT '验证邮箱验证码';
+--增加注册时间
+ALTER TABLE `user_base`  ADD `regtime` INT NOT NULL COMMENT '注册时间';
+--2016/08/31
+--增加数据表存放验证码
+--user:aunhappy
+CREATE TABLE IF NOT EXISTS `user_code` (
+  `id` int(11) NOT NULL COMMENT '用户ID',
+  `getpasstime` int(11) NOT NULL COMMENT '发送验证码时间',
+  `code` int(11) NOT NULL COMMENT '验证码',
+  `difference` int(11) NOT NULL COMMENT '区别',
+  `used` int(11) NOT NULL COMMENT '是否使用过',
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=gbk;
