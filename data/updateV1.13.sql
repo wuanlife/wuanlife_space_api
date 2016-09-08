bf333c5e7c8543b9ed547403063aa3d831068fa4
aunhappy <1195417752@qq.com>
2016-09-08 20:25:36 +0800

    数据库文件小修改

--- data/updateV1.13.sql
+++ data/updateV1.13.sql
@@ -10,10 +10,19 @@ ALTER TABLE `group_base`  ADD `private` INT(1) NOT NULL DEFAULT '0' COMMENT '私
 -- 新增消息基础表，包括消息id和消息内容
 -- user:小超
 CREATE TABLE IF NOT EXISTS `message_base` (
-  `id` int(4) NOT NULL COMMENT '消息id',
+  `code` varchar(4) NOT NULL COMMENT '消息码',
   `content` varchar(30) NOT NULL COMMENT '消息内容',
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`code`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- 转存表中的数据 `message_base`
+--
+
+INSERT INTO `message_base` (`code`, `content`) VALUES
+('0001', '{0} 申请加入{1}星球。'),
+('0002', '{0}同意你加入{1}星球。'),
+('0003', '你申请加入{1}星球已被拒绝。');
 -- 2016/09/08  13:18
 -- 新增消息细节表，包括消息id、用户id、创建时间、是否已读
 -- user:小超
