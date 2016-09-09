e8bacd92e1fca7679534254805d2fcd98a387d5a
aunhappy <1195417752@qq.com>
2016-09-09 20:29:00 +0800

    消息中心的数据库设计

--- data/updateV1.13.sql
+++ data/updateV1.13.sql
@@ -20,19 +20,18 @@ CREATE TABLE IF NOT EXISTS `message_base` (
 --
 
 INSERT INTO `message_base` (`code`, `content`) VALUES
-('0001', '{0} 申请加入{1}星球。'),
+('0001', '{0}申请加入{1}星球。'),
 ('0002', '{0}同意你加入{1}星球。'),
-('0003', '你申请加入{1}星球已被拒绝。');
+('0003', '你申请加入{1}星球已被{0}拒绝。');
 -- 2016/09/08  13:18
 -- 新增消息细节表，包括消息id、用户id、创建时间、是否已读
 -- user:小超
 CREATE TABLE IF NOT EXISTS `message_detail` (
   `message_base_id` int(4) NOT NULL COMMENT '消息id',
   `user_base_id` int(4) NOT NULL COMMENT '用户id',
-  `id_1` int(9) NOT NULL COMMENT '申请人或回复人id',
+  `id_1` int(9) NOT NULL COMMENT '申请人或创建人id',
   `id_2` int(9) NOT NULL COMMENT '星球id',
   `createTime` int(10) NOT NULL COMMENT '创建时间',
   `read` int(1) NOT NULL DEFAULT '0' COMMENT '是否已读',
-  `count` int(1) NOT NULL DEFAULT '1' COMMENT '区分申请回复',
-  PRIMARY KEY (`message_base_id`,`user_base_id`,`count`)
+  PRIMARY KEY (`message_base_id`,`user_base_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
