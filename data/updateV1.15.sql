b1f078fc9a1ffae6155496078ce5baefdff823f9
aunhappy <1195417752@qq.com>
2016-11-15 08:02:48 +0800

    V1.15数据库文件

---
+++ data/updateV1.15.sql
@@ -0,0 +1,22 @@
+-- 2016/11/15   17:29
+-- 新增用户收藏表
+-- user:小超
+CREATE TABLE
+IF NOT EXISTS `user_collection` (
+	`id` INT (11) NOT NULL AUTO_INCREMENT COMMENT '收藏id',
+	`post_base_id` INT (11) NOT NULL COMMENT '帖子id',
+	`group_base_id` INT (11) NOT NULL COMMENT '星球id',
+	`collected` TINYINT (1) NOT NULL COMMENT '是否收藏',
+	`time` INT (10) NOT NULL COMMENT '收藏时间',
+	`delete` TINYINT (1) NOT NULL COMMENT '是否删除',
+	PRIMARY KEY (`id`)
+) ENGINE = INNODB DEFAULT CHARSET = utf8 COLLATE = utf8_bin COMMENT = '用户收藏表' AUTO_INCREMENT = 1;
+-- 2016/11/15   17:29
+-- 修改用户细节表上次登录字段
+-- user:小超
+ALTER TABLE `user_detail` CHANGE `lastLogTime` `lastLogTime` INT (10) NOT NULL COMMENT '上次登录';
+-- 2016/11/15   17:29
+-- 修改用户细节表新增用户头像字段
+-- user:小超
+ALTER TABLE `user_detail` ADD `profile_picture` VARCHAR (255) CHARACTER
+SET utf8 COLLATE utf8_bin NOT NULL COMMENT '用户头像';
