a04674692d78f434e140c483b20e890a990f5256
aunhappy <1195417752@qq.com>
2016-11-16 15:12:14 +0800

    数据库修改

--- data/updateV1.15.sql
+++ data/updateV1.15.sql
@@ -5,7 +5,7 @@ CREATE TABLE
 IF NOT EXISTS `user_collection` (
 	`id` INT (11) NOT NULL AUTO_INCREMENT COMMENT '收藏id',
 	`post_base_id` INT (11) NOT NULL COMMENT '帖子id',
-    `user_base_id` INT (11) NOT NULL COMMENT '帖子id',
+    `user_base_id` INT (11) NOT NULL COMMENT '用户id',
 	`createTime` INT (10) NOT NULL COMMENT '收藏时间',
 	`delete` TINYINT (1) NOT NULL DEFAULT '0'  COMMENT '是否删除',
 	PRIMARY KEY (`id`)
@@ -18,4 +18,4 @@ ALTER TABLE `user_detail` CHANGE `lastLogTime` `lastLogTime` INT (10) NOT NULL C
 -- 修改用户细节表新增用户头像字段
 -- user:小超
 ALTER TABLE `user_detail` ADD `profile_picture` VARCHAR (255) CHARACTER
-SET utf8 COLLATE utf8_bin NOT NULL COMMENT '用户头像';
+SET utf8 COLLATE utf8_bin NULL COMMENT '用户头像';
