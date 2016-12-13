946ac960048d38d05b30017de88ce5c3ebf32695
aunhappy <1195417752@qq.com>
2016-12-13 11:33:20 +0800

    V1.16数据表

--- data/wuan.sql
+++ data/wuan.sql
@@ -218,12 +218,30 @@ CREATE TABLE IF NOT EXISTS `user_detail` (
 --
 -- user:小超
 
-CREATE TABLE `wuan`.`message_text` (
+CREATE TABLE IF NOT EXISTS `message_text` (
     `message_detail_id` INT (11) NOT NULL COMMENT '消息ID',
     `text` VARCHAR (65) CHARACTER
 SET utf8 COLLATE utf8_bin NOT NULL COMMENT '申请信息'
 ) ENGINE = INNODB CHARACTER
 SET utf8 COLLATE utf8_bin COMMENT = '消息内容表';
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `user_collection`
+--
+-- 创建时间： 2016/11/15   17:29
+--
+-- user:小超
+
+CREATE TABLE IF NOT EXISTS `user_collection` (
+	`post_base_id` INT (11) NOT NULL COMMENT '帖子id',
+    `user_base_id` INT (11) NOT NULL COMMENT '用户id',
+	`createTime` INT (10) NOT NULL COMMENT '收藏时间',
+	`delete` TINYINT (1) NOT NULL DEFAULT '0'  COMMENT '是否删除',
+	PRIMARY KEY (`post_base_id`,`user_base_id`)
+) ENGINE = INNODB DEFAULT CHARSET = utf8 COLLATE = utf8_bin COMMENT = '用户收藏表' AUTO_INCREMENT = 1;
+
 
 --
 -- Indexes for dumped tables
@@ -317,3 +335,20 @@ ALTER TABLE `post_image`
 --
 ALTER TABLE `user_base`
   MODIFY `id` int(5) unsigned NOT NULL AUTO_INCREMENT COMMENT '用户id',AUTO_INCREMENT=1;
+  
+-- 2016/11/15   17:29
+-- 修改用户细节表上次登录字段
+-- user:小超
+ALTER TABLE `user_detail` CHANGE `lastLogTime` `lastLogTime` INT (10) NOT NULL COMMENT '上次登录';
+-- 2016/11/15   17:29
+-- 修改用户细节表新增用户头像字段
+-- user:小超
+ALTER TABLE `user_detail` ADD `profile_picture` VARCHAR (255) CHARACTER
+SET utf8 COLLATE utf8_bin NULL COMMENT '用户头像';
+
+INSERT INTO `wuan`.`message_base` (`code`, `type`, `content`)
+VALUES
+	('0004','3','{0}已将你从{1}中移除'),
+	('0005','3','{0}已从你的{1}中退出'),
+	('0006','3','{0}已加入你的{1}'),
+	('0007','3','{0}回复我的主题{1}');
