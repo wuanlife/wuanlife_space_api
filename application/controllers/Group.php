5042baf6ec33e249f5d094ce73d5bce9bf00b318
jinchaojian <1139883346@qq.com>
2017-03-05 14:17:04 +0800

    配置接口

--- application/controllers/Group.php
+++ application/controllers/Group.php
@@ -4,5 +4,46 @@
 
 class Group extends CI_Controller
 {
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->model('Group_model');
+        $this->load->helper('url_helper');
+    }
+    public function response($data,$ret=200,$msg=null){
+        $response=array('ret'=>$ret,'data'=>$data,'msg'=>$msg);
+        $this->output
+            ->set_status_header($ret)
+            ->set_header('Cache-Control: no-store, no-cache, must-revalidate')
+            ->set_header('Pragma: no-cache')
+            ->set_header('Expires: 0')
+            ->set_content_type('application/json', 'utf-8')
+            ->set_output(json_encode($response))
+            ->_display();
+        exit;
+    }
+
+
+
+    public function create($user_id,$g_name,$g_image,$g_introduction,$private){
+        $private=$this->Group_model->judgePrivate($private);
+        $data=array(
+            'user_id'=>$user_id,
+            'g_name'=>$g_name,
+            'g_image'=>$g_image,
+            'g_introduction'=>$g_introduction,
+            'private'=>$private,
+        );
+        $msg=null;
+        $re=$this->Group_model->create($data);
+        $this->response($re,200,$msg);
+
+    }
+
+
+
+
+
+
 
 }
--- application/models/Group_model.php
+++ application/models/Group_model.php
@@ -4,5 +4,32 @@
 
 class Group_model extends CI_Model
 {
+    public function __construct()
+    {
+        $this->load->database();
+    }
+
+    public function create($data){
+
+    }
+
+
+
+
+    /*
+     * 判断是否为私密
+     */
+    public function judgePrivate($private){
+        if($private==1){
+            $private=1;
+        }else{
+            $private=0;
+        }
+
+        return $private;
+    }
+
+
+
 
 }
