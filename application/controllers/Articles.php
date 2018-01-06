b25dff171aa4587b1edcc5a16c10e049cf8b3f4a
阿萌 <admin@wangmengyu.cn>
2018-01-07 05:18:24 +0800

    完成文章 A6、A7、A8、A9（剩下权限提示）

--- application/controllers/Articles.php
+++ application/controllers/Articles.php
@@ -11,8 +11,33 @@ class Articles extends REST_Controller
     public function __construct(string $config = 'rest')
     {
         parent::__construct($config);
-
+        $this->load->model('articles_model');
     }
+
+//    /**
+//     * 解析jwt，获得用户id（旧的拷贝过来的）
+//     * @param $jwt
+//     * @return mixed
+//     */
+//    private function parsing_token($jwt)
+//    {
+//        try{
+//            $token = $this->jwt->decode($jwt,$this->config->item('encryption_key'));
+//            return $token;
+//        }
+//        catch(InvalidArgumentException $e)
+//        {
+//            return $this->response(['error'=>'未登录，不能操作'],401);
+//        }
+//        catch(UnexpectedValueException $e)
+//        {
+//            return $this->response(['error'=>'未登录，不能操作'],401);
+//        }
+//        catch(DomainException $e)
+//        {
+//            return $this->response(['error'=>'未登录，不能操作'],401);
+//        }
+//    }
 
     /**
      * 搜索文章
@@ -31,5 +56,174 @@ class Articles extends REST_Controller
         $this->response($data, 200);
     }
 
+    /**
+     * 发表文章/发表评论/编辑文章/删除文章评论
+     * @param null $aid
+     * @param null $type
+     * @param null $floor
+     */
+    public function index_post($aid = null, $type = null, $floor = null): void
+    {
+//        //校验权限
+//        $token = $this->input->get_request_header('Access-Token', TRUE);
+//        $user_info = $this->parsing_token($token);
+
+        //处理URL变量
+        $aid_null = is_null($aid);
+        $type_null = is_null($type);
+        $floor_null = is_null($floor);
+        $aid = intval($aid);
+        $floor = intval($floor);
+
+        if($aid_null && $type_null && $floor_null){      //发表文章
+            /* 获取POST数据 */
+            $title = trim($this->input->post('title'));
+            $content = trim($this->input->post('content'));
+            $content_txt = str_replace('&nbsp;','',strip_tags($content));
+            $image_urls = $this->input->post('image_urls');
+            $image_urls_arr = explode(',', $image_urls);
+
+            //验证POST数据
+            !empty($title) or $this->response(['error'=>'文章标题不能为空'], 400);
+            mb_strlen($title) <= 60 or $this->response(['error'=>'标题不能超过60个字符'], 400);
+            !empty($content) or $this->response(['error'=>'文章正文不能为空'], 400);
+            mb_strlen($content_txt) <= 5000 or $this->response(['error'=>'文章正文不能超过5000个字符'], 400);
+            count($image_urls_arr)<=3 or $this->response(['error'=>'至多三张预览图片'], 400);
+
+            //组合数据
+            $data = [
+                'user_id'=>$user_info->user_id,
+                'user_name'=>$user_info->user_name,
+                'title'=>$title,
+                'content'=>$content,
+                'resume'=>substr($content_txt,0,90).'...',
+                'image_urls_arr'=>$image_urls_arr
+            ];
+
+            $result['id'] = $this->articles_model->articleAdd($data);
+            if($result['id'] > 0){
+                $this->response($result, 200);
+            }else{
+                $this->response(['error'=>'创建失败'], 400);
+            }
+        }else if(!$aid_null && $type_null && $floor_null){      //编辑文章
+            /* 获取POST数据 */
+            $title = trim($this->input->post('title'));
+            $content = trim($this->input->post('content'));
+            $content_txt = str_replace('&nbsp;','',strip_tags($content));
+
+            //验证POST数据
+            !empty($title) or $this->response(['error'=>'文章标题不能为空'], 400);
+            mb_strlen($title) <= 60 or $this->response(['error'=>'标题不能超过60个字符'], 400);
+            !empty($content) or $this->response(['error'=>'文章正文不能为空'], 400);
+            mb_strlen($content_txt) <= 5000 or $this->response(['error'=>'文章正文不能超过5000个字符'], 400);
+
+            //组合数据
+            $data = [
+                'id'=>$aid,
+                'title'=>$title,
+                'content'=>$content,
+                'resume'=>substr($content_txt,0,90).'...'
+            ];
+
+            $result['id'] = $this->articles_model->articleUpd($data);
+            if($result['id'] > 0){
+                $this->response($result, 200);
+            }else{
+                $this->response(['error'=>'修改失败'], 400);
+            }
+        }else if(!$aid_null && $type=='comments' && $floor_null){      //评论文章
+            /* 获取POST数据 */
+            $comment = trim($this->input->post('comment'));
+
+            //验证POST数据
+            !empty($comment) or $this->response(['error'=>'回复内容不能为空'], 400);
+            mb_strlen($comment) <= 5000 or $this->response(['error'=>'回复内容不能超过5000个字符'], 400);
+
+            //组合数据
+            $data = [
+                'user_id'=>1,
+                'comment'=>'1111',
+                'article_id'=>$aid,
+            ];
+            $result = $this->articles_model->commentsAdd($data);
+            if($result){
+                $result['user']['id'] = $user_info->user_id;
+                $result['user']['name'] = $user_info->user_name;
+                $this->response($result, 200);
+            }else{
+                $this->response(['error'=>'评论失败'], 400);
+            }
+        }
+    }
+
+    public function index_put($aid = null, $type = null, $floor = null): void
+    {
+        //处理URL变量
+        $aid_null = is_null($aid);
+        $type_null = is_null($type);
+        $floor_null = is_null($floor);
+        $aid = intval($aid);
+        $floor = intval($floor);
+
+        if(!$aid_null && $type_null && $floor_null){      //编辑文章
+            /* 获取POST数据 */
+            $title = trim($this->input->input_stream('title'));
+            $content = trim($this->input->input_stream('content'));
+            $content_txt = str_replace('&nbsp;','',strip_tags($content));
+
+            //验证POST数据
+            !empty($title) or $this->response(['error'=>'文章标题不能为空'], 400);
+            mb_strlen($title) <= 60 or $this->response(['error'=>'标题不能超过60个字符'], 400);
+            !empty($content) or $this->response(['error'=>'文章正文不能为空'], 400);
+            mb_strlen($content_txt) <= 5000 or $this->response(['error'=>'文章正文不能超过5000个字符'], 400);
+
+            //组合数据
+            $data = [
+                'id'=>$aid,
+                'title'=>$title,
+                'content'=>$content,
+                'resume'=>substr($content_txt,0,90).'...'
+            ];
+
+            $result['id'] = $this->articles_model->articleUpd($data);
+            if($result['id'] > 0){
+                $this->response($result, 200);
+            }else{
+                $this->response(['error'=>'修改失败'], 400);
+            }
+        }
+    }
+
+    public function index_delete($aid = null, $type = null, $floor = null): void
+    {
+        //处理URL变量
+        $aid_null = is_null($aid);
+        $type_null = is_null($type);
+        $floor_null = is_null($floor);
+        $aid = intval($aid);
+        $floor = intval($floor);
+
+        if(!$aid_null && $type=='comments' && !$floor_null){        //删除评论
+            $data = [
+                'article_id'=>$aid,
+                'floor'=>$floor
+            ];
+            $id  = $this->articles_model->commentsOneId($data);
+            $id!=0 or $this->response(['error'=>'评论不存在'], 400);
+
+            $data = [
+                'article_id'=>$aid,
+                'floor'=>$floor
+            ];
+            $result = $result['id'] = $this->articles_model->commentsDel($id);
+            if($result){
+                $this->response(['error'=>'删除成功'], 200);
+            }else{
+                $this->response(['error'=>'删除失败'], 400);
+            }
+        }
+
+    }
 
 }
---
+++ application/models/Articles_model.php
@@ -0,0 +1,219 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: moe
+ * Date: 2018/1/6
+ * Time: 21:16
+ */
+
+class Articles_model extends CI_Model
+{
+    private $a_base = 'articles_base';
+    private $a_content = 'articles_content';
+    private $a_status = 'articles_status';
+    private $a_status_detail = 'a_status_detail';
+    private $a_approval = 'articles_approval';
+    private $a_approval_count = 'articles_approval_count';
+    private $a_comments = 'articles_comments';
+    private $a_comments_count = 'articles_comments_count';
+    private $c_contents = 'comment_contents';   //评论内容表
+    private $i_url = 'image_url';   //文章图片url表
+    public function __construct()
+    {
+        parent::__construct();
+    }
+    public function test(): array
+    {
+        $res = $this->db->select('*')->from($this->a_base)->get();
+        return $res->result_array();
+    }
+
+    /**
+     * 添加文章
+     * @param $data
+     * @return int
+     */
+    public function articleAdd($data): int
+    {
+        $id = 0;
+        //回滚 start
+        $this->db->trans_start();
+        //添加文章表
+        $data_base = [
+            'author_id'=>$data['user_id'],
+            'author_name'=>$data['user_name'],
+            'content_digest'=> $data['resume'],
+            'create_at'=>date('Y-m-d H:i:s')
+        ];
+        $this->db->insert($this->a_base, $data_base);
+        $id = $this->db->insert_id();
+
+        //添加文章内容表
+        $data_content = [
+            'id'=>$id,
+            'title'=>$data['title'],
+            'content'=>$data['content'],
+        ];
+        $this->db->insert($this->a_content, $data_content);
+
+        //添加文章状态表
+        $data_status = [
+            'id'=>$id,
+            'status'=>0,
+            'create_at'=>date('Y-m-d H:i:s')
+        ];
+        $this->db->insert($this->a_status, $data_status);
+
+        //添加文章图片url
+        $data_url = [];
+        foreach($data['image_urls_arr'] as $k => $v){
+            $data_url[$k] = [
+                'article_id'=>$id,
+                'url'=>$v
+            ];
+        }
+        $this->db->insert_batch($this->i_url, $data_url);
+
+        //结束 end
+        $this->db->trans_complete();
+        return $id;
+    }
+
+    /**
+     * 修改文章内容
+     * @param $data
+     * @return int
+     */
+    public function articleUpd($data): int
+    {
+        //回滚 start
+        $this->db->trans_start();
+
+        //添加文章表
+        $data_base_where = [
+            'id'=>$data['id'],
+        ];
+        $data_base_data = [
+            'content_digest'=>$data['resume'],
+        ];
+        $this->db->where($data_base_where)->update($this->a_base, $data_base_data);
+        $id = $this->db->insert_id();
+
+        //添加文章内容表
+        $data_content_where = [
+            'id'=>$data['id'],
+        ];
+        $data_content_data = [
+            'title'=>$data['title'],
+            'content'=>$data['content'],
+        ];
+        $this->db->where($data_content_where)->update($this->a_content, $data_content_data);
+
+        //结束 end
+        $this->db->trans_complete();
+        return $data['id'];
+    }
+
+    /**
+     * 获取文章评论数量缓存
+     * @param $id
+     * @return int
+     */
+    public function commentsCount($id): int
+    {
+        $res = $this->db->select('count')->from($this->a_comments_count)->where('articles_id', $id)->get();
+        $result = $res->row();
+        if(empty($result)){
+            return 0;
+        }else{
+            return $res->result()->count;
+        }
+    }
+
+    /**
+     * 获得评论id
+     * @param $data
+     * @return int
+     */
+    public function commentsOneId($data):int
+    {
+        $res = $this->db->select('comment_id')->from($this->a_comments)->where($data)->get();
+        $result = $res->row();
+        if(empty($result)){
+            return 0;
+        }else{
+            return $result->comment_id;
+        }
+    }
+
+    /**
+     * 添加评论
+     * @param $data
+     * @return array|bool
+     */
+    public function commentsAdd($data)
+    {
+        $id = 0;
+        //获得现有楼层
+        $count = $this->commentsCount($data['article_id']);
+
+        //回滚 start
+        $this->db->trans_start();
+
+        //添加文章评论表
+        $now_time = date('Y-m-d H:i:s');
+        $data_comments = [
+            'article_id'=>$data['article_id'],
+            'user_id'=>$data['user_id'],
+            'floor'=>$count+1,
+            'create_at'=>$now_time
+        ];
+        $res1 = $this->db->insert($this->a_comments, $data_comments);
+        $id = $this->db->insert_id();
+
+        //添加文章内容表
+        $data_c_contents = [
+            'id'=>$id,
+            'content'=>$data['comment']
+        ];
+        $res2 = $this->db->insert($this->c_contents, $data_c_contents);
+        //回滚 end
+        $this->db->trans_complete();
+
+        if($res1 && $res2){
+            $result = [
+                'comment'=>$data['comment'],
+                'update_at'=>$now_time,
+                'create_at'=>$now_time,
+                'floor'=>$count+1
+            ];
+            return $result;
+        }else{
+            return false;
+        }
+    }
+
+
+    public function commentsDel($id)
+    {
+        //回滚 start
+        $this->db->trans_start();
+
+        //删除评论基础信息
+        $data_comments = [
+            'comment_id'=>$id
+        ];
+        $res1 = $this->db->where($data_comments)->delete($this->a_comments);
+
+        //删除文章评论数缓存信息
+        $data_contents = [
+            'id'=>$id
+        ];
+        $res2 = $this->db->where($data_contents)->delete($this->c_contents);
+
+        //回滚 end
+        $this->db->trans_complete();
+
+        return ($res1&&$res2);
+    }
+}
