2fda79cd5ed718108eb5f62237e9a8671ab1e714
Gtacer <459472218@qq.com>
2018-01-07 22:47:37 +0800

    将搜索文章功能移到Search类中

--- application/controllers/Articles.php
+++ application/controllers/Articles.php
@@ -33,23 +33,6 @@ class Articles extends REST_Controller
     }
 
     /**
-     * 搜索文章
-     */
-    public function search_post(): void
-    {
-        // 加载搜索模版
-        $this->load->model('search_model');
-        // 获取url参数列表
-        $param = $this->search_model->getSearchParam();
-        // 判断参数列表是否完整
-        $this->search_model->validateSearchParam($param) or $this->response(['error' => '缺少必要的参数'], 400);
-        // 获取相匹配的数据
-        $data = $this->search_model->search($param, 'articles');
-        // 返回数据
-        $this->response($data, 200);
-    }
-
-    /**
      * 发表文章/发表评论
      * @param null $aid
      * @param null $type
