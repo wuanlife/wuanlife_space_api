4f5939981719f2687f55002445c3180f0eec4166
Gtacer <459472218@qq.com>
2018-01-08 19:29:51 +0800

    预设管理员对应关系

--- data/v1.3.0 Update.sql
+++ data/v1.3.0 Update.sql
@@ -1,12 +1,15 @@
 USE wuan_api_new;
 
 -- 设置文章状态对应关系
-INSERT INTO articles_status_detail VALUES (1,'锁定'),(2,'删除');
+INSERT INTO articles_status_detail VALUES (1, '锁定'), (2, '删除');
 
 -- 为超级管理员占坑
-INSERT INTO users_base VALUES (1,'11111','11111',md5(123456),DEFAULT );
-INSERT INTO avatar_url VALUES (1,'11111',0);
-INSERT INTO users_detail VALUES (1,3,'0000-00-00');
+INSERT INTO users_base VALUES (1, '11111', '11111', md5(123456), DEFAULT);
+INSERT INTO avatar_url VALUES (1, '11111', 0);
+INSERT INTO users_detail VALUES (1, 3, '0000-00-00');
 
 -- 添加性别对应关系
-INSERT INTO sex_detail VALUES (0,'女'),(1,'男'),(3,'不想透露');
+INSERT INTO sex_detail VALUES (0, '女'), (1, '男'), (3, '不想透露');
+
+-- 添加管理员对应关系
+INSERT INTO auth_detail VALUES (1, '超级管理员'), (2, '管理员');
