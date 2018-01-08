e5f83bdf4f8841e6c69173753bc509bb3eb8ae38
Gtacer <459472218@qq.com>
2018-01-08 18:07:18 +0800

    数据库更新文件

---
+++ data/v1.3.0 Update.sql
@@ -0,0 +1,11 @@
+USE wuan_api_new;
+
+-- 设置文章状态对应关系
+INSERT INTO articles_status_detail VALUES (1,'锁定'),(2,'删除');
+
+-- 为超级管理员占坑
+INSERT INTO users_base VALUES (1,'11111','11111',md5(123456),DEFAULT );
+INSERT INTO avatar_url VALUES (1,'11111',0);
+
+-- 添加性别对应关系
+INSERT INTO sex_detail VALUES (0,'女'),(1,'男'),(3,'不想透露');
