692568b4d3bc11b8f0f50faa0bd75d7c919930a6
gitlwy <jyh120@outlook.com>
2018-01-11 00:44:55 +0800

    A3完成

--- application/config/routes.php
+++ application/config/routes.php
@@ -64,6 +64,7 @@ $route['users/(:num)']['get'] = 'users/users/$1';
 $route['users/(:num)']['put'] = 'users/users/$1';
 $route['users/(:num)/password']['put'] = 'users/password/$1';
 $route['users/(:num)/collections']['get'] = 'users/collections/$1';
+$route['users/(:num)/article']['get'] = 'users/article/$1';
 
 //搜索相关路由
 $route['users/search']['post'] = 'search/users';
