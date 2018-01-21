5fff635f31b0db3bc65051254379ea1777cf19db
Gtacer <459472218@qq.com>
2018-01-22 00:31:01 +0800

    更正bug

--- application/models/Users_model.php
+++ application/models/Users_model.php
@@ -100,7 +100,7 @@ class Users_model extends CI_Model
             ->from('users_base')
             ->join('avatar_url', 'users_base.id = avatar_url.user_id', 'left')
             ->join('users_detail', 'users_base.id = users_detail.id', 'left')
-            ->where(['users_base.id' => $id, 'url' => 0])
+            ->where(['users_base.id' => $id])
             ->get();
 
         $result = $res->result()[0];
