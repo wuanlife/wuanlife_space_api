3562891a2c1098c30cfb2d287b1ab96cf8fdd7e0
Gtacer <459472218@qq.com>
2018-02-01 17:40:22 +0800

    修正where方法的用法

--- application/models/Articles_model.php
+++ application/models/Articles_model.php
@@ -622,7 +622,7 @@ class Articles_model extends CI_Model
         $this->db->join('articles_content',' articles_content.id = articles_base.id');
         $this->db->join('users_base','users_base.name = articles_base.author_name');
         $this->db->join('articles_status','articles_status.id = articles_base.id','left');
-        $this->db->where("((articles_status.status>>2)&1)!=0"); //被删除的文章不显示，更正判断删除的逻辑，Gtaker 2018/2/1   17:20
+        $this->db->where('((articles_status.status >> 2) & 1) !=','0'); //被删除的文章不显示，更正判断删除的逻辑，Gtaker 2018/2/1   17:20
         $re['total'] = $this->db->get()->num_rows();
         //删除不需要返回的
         foreach ($re['articles'] as $key => $value) {
